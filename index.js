3f1418aed0b977062060108e6f979840f8f0a001
K2 <96985860+Arleannez@users.noreply.github.com>
2024-08-20 08:18:11 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const elements = document.querySelectorAll('.fade-in');
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: .4
+}
+const callbacks = (entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting){
+      entry.target.classList.add('active');
+    }
+  });
+}
+let observer = new IntersectionObserver(callbacks, options);
+elements.forEach(element => {
+  observer.observe(element);
+});
+
+/*navigation*/
+
+
+const images = document.querySelectorAll('.columnpics img');
+
+images.forEach((image) => {
+  image.addEventListener('click', () => {
+    image.classList.toggle('enlarged');
+  });
+});
